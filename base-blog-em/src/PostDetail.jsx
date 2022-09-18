7d8ca661cbb20f814940051efe69b6c6a9f394cf
crinkj <jcrinkey89120@gmail.com>
2022-09-19 00:50:13 +0900

    feat:포스팅 상세보기 구현

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,23 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const {data, isError, error, isLoading} = useQuery(
+      ["comments",post.id], 
+      () => fetchComments(post.id)
+    );
+  if(isError)
+  return (
+    <>
+      <h3 style={{color:"red"}}>error is occurred(reason: {error.toString()})</h3>
+    </>
+  )
+
+  if(isLoading)
+  return (
+    <>
+      <h3 style={{color:"blue"}}>bruh, it takes forever</h3>
+    </>
+  )
 
   return (
     <>
